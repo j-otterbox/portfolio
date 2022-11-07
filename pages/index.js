51460394f23281dc894f29bd1a8611d9461bbfaa
Jason Otter <jasonotter17@gmail.com>
2022-11-06 23:36:43 -0800

    add google fonts link

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,12 @@ export default () => {
           name="description"
           content={description || "Gatsby Simplefolio"}
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </Helmet>
       <div className="app">
         <App />
